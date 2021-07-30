e3871678578184f5de278f827f276c07b0442942
Arturo P <arturoperezfigueroa@gmail.com>
2021-07-30 11:47:36 -0500

    Diseño popup e interaccion

--- js/popup.js
+++ js/popup.js
@@ -1,2 +1,11 @@
+let closeIcon = document.getElementById('closeicon');
+let popUpContainer = document.getElementById('popUp-ctn');
+let ctaBtn = document.getElementById('cta-btn');
 
+ctaBtn.addEventListener('click', () => {
+    popUpContainer.classList.remove('display-none')
+})
 
+closeIcon.addEventListener('click', () => {
+    popUpContainer.classList.add('display-none')
+})
